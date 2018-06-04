924becd917cf0ebf1a80b6dffabd8f2ed267fc46
ale1307 <ale1307@users.noreply.github.com>
2018-06-04 13:00:18 -0600

    Semana2

---
+++ Ejemplo2/src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,31 @@
+
+package ejemplo2;
+
+import java.util.Scanner;
+
+
+public class Ejemplo2 {
+
+    
+    public static void main(String[] args) {
+        int equipoA;
+        int equipoB;
+        int equipoC;
+        int equipoD;
+        int i;
+        
+        for (i > 8); {
+        
+    }
+                
+            
+        
+        
+           
+            
+            
+                
+    
+    }
+    
+}
