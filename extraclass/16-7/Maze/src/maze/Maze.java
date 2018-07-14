5ed5553f96aff4cbd4ada7bb98ddc78759e80683
ale1307 <ale1307@users.noreply.github.com>
2018-07-13 18:39:28 -0600

    6-17

---
+++ extraclass/16-7/Maze/src/maze/Maze.java
@@ -0,0 +1,22 @@
+package maze;
+
+import java.util.Scanner;
+
+public class Maze {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String maze[][]  = {
+            {1,1,1,1,1,1,1},
+            {1,0,0,0,0,0,3},
+            {1,0,1,0,1,0,1},
+            {0,0,1,0,0,0,1},       
+            {1,0,1,0,1,0,1},
+            {1,0,0,0,0,0,1},
+            {1,2,1,0,1,0,1}
+        };
+              
+
+    }
+
+}
